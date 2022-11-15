0dcdc003ce3d1e04e6640254f509274338c0b869
Edwin Young🐙 <edyoung@microsoft.com>
2022-11-14 21:47:15 -0800

    Initial Framework - Test installer - Unmanaged Custom  Action - NativeAOT Custom Action
    Both custom actions are called, the latter does nothing so far

---
+++ CustomAction/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CustomAction;
+public class Class1
+{
+    [UnmanagedCallersOnly(EntryPoint = "CustomAction2")]
+    public static uint CustomAction2(IntPtr handle)
+    {
+        return 0;
+    }
+}
